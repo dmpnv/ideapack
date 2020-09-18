b073692904de073abf7a752e483e53c80fd3e877
dp <dp@localhost>
2020-09-18 23:18:13 +0200

    initial project state

---
+++ ideapack-services/src/main/java/com/dmpnv/ideapack/services/IdeapackServicesApplication.java
@@ -0,0 +1,13 @@
+package com.dmpnv.ideapack.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeapackServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeapackServicesApplication.class, args);
+	}
+
+}
---
+++ ideapack-services/src/test/java/com/dmpnv/ideapack/services/IdeapackServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmpnv.ideapack.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeapackServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
