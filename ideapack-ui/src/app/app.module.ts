54bf2d4e93492ac39a8d346b417378922971ec60
dp <dp@localhost>
2020-09-30 00:42:25 +0200

    dev: layout components

--- ideapack-ui/src/app/app.module.ts
+++ ideapack-ui/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import {LayoutModule} from './layout/layout.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ideapack-ui/src/app/layout/layout.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+  declarations: [
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  exports: [
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class LayoutModule { }
